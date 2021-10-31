4821d046a0ec2f87a09f3cacafb5d9aac384dc99
zlwqa <asdori1369@gmail.com>
2021-10-31 21:33:00 +0400

    fix

---
+++ src/main/java/com/github/zlw/Main.java
@@ -0,0 +1,22 @@
+package com.github.zlw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book lagom = new Book();
+        lagom.nameBook = "Lagom";
+        lagom.totalCountOfPages = 200;
+        lagom.countOfPagesRead = 200;
+        lagom.sayNameBook();
+        lagom.readToLeft();
+
+        Book uve = new Book();
+        uve.nameBook = "Uve";
+        uve.totalCountOfPages = 490;
+        uve.countOfPagesRead = 380;
+        uve.sayNameBook();
+        uve.readToLeft();
+    }
+
+
+}
